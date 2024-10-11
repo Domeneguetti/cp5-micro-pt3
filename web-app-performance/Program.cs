165f475cec0fdbbb93e9203d05479cbb361e8db1
Domeneguetti <domeneguettidaniel@gmail.com>
2024-10-10 23:04:34 -0300

    first commit

---
+++ web-app-performance/Program.cs
@@ -0,0 +1,32 @@
+using web_app_repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//no web-app-performance é necessario importar os dois arquivos no import de projetos na dependencias
+//Configurar injecao da dependencia
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
